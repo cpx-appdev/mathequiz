819ca54c98fa40b9cae4113b9119150027df6ddf
Markus Langpeter <markus.langpeter@comparex.de>
2018-02-02 09:32:17 +0100

    Projektdateien hinzufügen.

---
+++ Permutationen/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutationen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Eigentlich auf einem Blatt Papier gelöst! Hier aber nochmal die Herangehensweise:
+
+            var gesuchtePermutation = 999999; // inkl. 0
+            var zahlen = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            while (zahlen.Any())
+            {
+                // Berechne Anzahl der Permutationen der restlichen Zahlen abzüglich der gesuchten Zahl = (n-1)!
+                var fact = 1;
+                for (var i = 1; i <= zahlen.Count - 1; i++)
+                {
+                    fact *= i;
+                }
+
+                // Ganzzahlige Division mit gesuchtePermutation=Rest
+                var idx = gesuchtePermutation / fact;
+                gesuchtePermutation = gesuchtePermutation - idx * fact;
+
+                var ziffer = zahlen[idx];
+                zahlen.RemoveAt(idx);
+
+                Console.Write(ziffer);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Primzahl/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Primzahl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aktuelleZahl = 1, n = 1;
+            while (true)
+            {
+                aktuelleZahl += 2;
+                var m = (int)Math.Ceiling(Math.Sqrt(aktuelleZahl));
+                bool isPrim = true;
+                for (var d = 2; d <= m; d++)
+                {
+                    if (aktuelleZahl % d == 0)
+                    {
+                        isPrim = false;
+                        break;
+                    }
+                }
+                if (isPrim)
+                {
+                    n++;
+                    Console.WriteLine($"{n}: {aktuelleZahl}");
+                    if (n == 10001)
+                        Console.ReadLine();
+                }
+            }
+        }
+    }
+}
